25cb9d562c7bccebd0732e767ee316d519c7666f
warunalakshitha <warudanu@gmail.com>
2025-02-02 01:17:19 +0530

    Add initial compiler code

---
+++ src/main/java/org/waruna/compiler/Main.java
@@ -0,0 +1,8 @@
+package org.waruna.compiler;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("This is simple arithmetic compiler");
+    }
+}
