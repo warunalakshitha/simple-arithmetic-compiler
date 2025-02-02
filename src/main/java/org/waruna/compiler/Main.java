5f3cc53a2745d34b9dc59db46f66117348b5e782
warunalakshitha <warudanu@gmail.com>
2025-02-02 14:53:31 +0530

    Add bytecode save logic

--- src/main/java/org/waruna/compiler/Main.java
+++ src/main/java/org/waruna/compiler/Main.java
@@ -1,7 +1,10 @@
 package org.waruna.compiler;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
+import org.waruna.compiler.codegen.CodeGenerator;
 import org.waruna.compiler.model.Node;
 import org.waruna.compiler.model.Token;
 import org.waruna.compiler.parse.Lexer;
@@ -18,6 +21,16 @@ public class Main {
         List<Token> tokens = lexer.tokenize(args[0]);
         Parser parser = new Parser();
         Node parseTree = parser.parse(tokens);
-        System.out.println(parseTree);
+        CodeGenerator codeGenerator = new CodeGenerator();
+        byte[] bytecode = codeGenerator.generateBytecode(parseTree, "MyProgram");
+        saveClassToFile(bytecode, "MyProgram.class");
+    }
+
+      public static void saveClassToFile(byte[] bytecode, String filename) {
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            fos.write(bytecode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
